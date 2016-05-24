07f62c20ca27619ee9ecf2d13b6e5201e45e38e5
cahrens <christina@edx.org>
2016-05-24 14:32:42 -0400

    See how often it fails.

--- common/test/acceptance/tests/studio/test_studio_split_test.py
+++ common/test/acceptance/tests/studio/test_studio_split_test.py
@@ -1045,7 +1045,7 @@ class GroupConfigurationsTest(ContainerBase, SplitTestMixin):
         rendered_group_names = self.get_select_options(page=courseware_page, selector=".split-test-select")
         self.assertListEqual(group_names, rendered_group_names)
 
-    @flaky  # TODO TNL-4561
+    @flaky(20, 20)
     def test_split_test_LMS_staff_view(self):
         """
         Scenario: Ensure that split test is correctly rendered in LMS staff mode as it is
